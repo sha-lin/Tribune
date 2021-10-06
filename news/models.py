4625ba7f0beb929e84a0d285d733aee0168a1f19
shalin <shalin.rono@student.moringaschool.com>
2021-10-06 20:06:18 +0300

    make articles changes

--- news/models.py
+++ news/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
+
+    def __str__(self):
+        return self.first_name
+    class meta:
+        ordering =['name']
+    
+    def save_editor(self):
+        self.save()
+
+
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length =60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor,on_delete=models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to = 'articles/')
+
+    def test_get_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
+        
+        @classmethod
+        def days_news(cls,date):
+            news = cls.objects.filter(pub_date__date = date)
+            return news
+
+        @classmethod
+        def search_by_title(cls,search_term):
+            news = cls.objects.filter(title__icontains=search_term)
+            return news
+
+    
